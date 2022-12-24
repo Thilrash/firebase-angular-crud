d6206c977e3eb35f42bbb4afc1ec265cf1c70c9a
Thilrash <ameenthilrash@gmail.com>
2022-12-24 21:42:54 +0530

    Added authenticatiion service

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
+  firebase: {
+    projectId: 'student-managment-system-831d1',
+    appId: '1:65658766714:web:6bd76e95759b022ea042c3',
+    storageBucket: 'student-managment-system-831d1.appspot.com',
+    apiKey: 'AIzaSyCQV9i3V5aykPHSvPWrhqvU85k30E6U1tI',
+    authDomain: 'student-managment-system-831d1.firebaseapp.com',
+    messagingSenderId: '65658766714',
+  },
   production: true
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,6 +3,14 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
+  firebase: {
+    projectId: 'student-managment-system-831d1',
+    appId: '1:65658766714:web:6bd76e95759b022ea042c3',
+    storageBucket: 'student-managment-system-831d1.appspot.com',
+    apiKey: 'AIzaSyCQV9i3V5aykPHSvPWrhqvU85k30E6U1tI',
+    authDomain: 'student-managment-system-831d1.firebaseapp.com',
+    messagingSenderId: '65658766714',
+  },
   production: false
 };
 
